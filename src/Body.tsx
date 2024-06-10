63b4fd1530c64fa967f29e3d6d63462e650cde28
Davide Del Bianco <davidedelbianco@IHD00080.local>
2024-06-10 15:16:09 +0200

    modifiche sul costruttore di body e modalità di visualizzazione

--- src/Body.tsx
+++ src/Body.tsx
@@ -1,14 +1,25 @@
-import { useState } from 'react'
-type BodyProps = {risposta:string}
-function Body(Par:BodyProps) {
-    
-return (
+type messaggio = {textToAdd: string; senderMess: boolean}       //classe che inserisce il messaggio (textToAdd) con le caratteristiche visuali che dipendono dal mittente I/O o no
 
-    <>
-        <textarea id="casellaRisposta" value={Par.risposta}> </textarea>
-        
-    </>
-);
+function Body(mess:messaggio) {
+    function controllaTipo():string             //funzione che distingue i vari tipi di Id in caso di specifica modifica
+    {
+        let tipo: string =""
+        if(mess.senderMess)
+            tipo = "idUtente"
+        else 
+            tipo = "idComput"
+        return tipo
+    }
+
+    return (
+
+        <>
+            <p id={controllaTipo()}>
+                {mess.textToAdd}
+            </p>
+            
+        </>
+    );
 }
 
 export default Body
